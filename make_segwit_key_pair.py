004e8662487e0f927532bd948c11a09b98412544
sinku <sininoir@gmail.com>
2021-05-18 01:33:41 +0430

    added test code to make segwit address keypair

---
+++ make_segwit_key_pair.py
@@ -0,0 +1,8 @@
+import key_maker
+
+if __name__ == '__main__':
+    private_key = key_maker.get_private_key()
+    wif = key_maker.get_wif_key(private_key)
+    compressed_key = key_maker.get_compressed_public_key(private_key)
+    segwit_address = key_maker.get_segwit_address(compressed_key)
+    print(f'wifAdd: {wif}\nsegwit: {segwit_address}')

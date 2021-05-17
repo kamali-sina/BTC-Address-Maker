f174a794ab8b31edc79fe3cf1a5a755ad1cfddd6
sinku <sininoir@gmail.com>
2021-05-18 01:33:06 +0430

    added test code to make normal keypair

---
+++ make_normal_key_pair.py
@@ -0,0 +1,8 @@
+import key_maker
+
+if __name__ == '__main__':
+    private_key = key_maker.get_private_key()
+    wif = key_maker.get_wif_key(private_key)
+    compressed_key = key_maker.get_compressed_public_key(private_key)
+    wallet_address = key_maker.get_wallet_address(compressed_key)
+    print(f'wif: {wif}\npub: {wallet_address}')
